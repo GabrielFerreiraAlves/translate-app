f6ef3c84e8167d6e3b7b6af302c8a61857a542f3
Gabriel Ferreira <96392170+GabrielFerreiraAlves@users.noreply.github.com>
2024-03-03 13:32:01 -0300

    update

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,29 +1,27 @@
-import Select from 'react-select'
-import { languages } from '../lib/languages.ts'
+import Select from 'react-select';
+import { languages } from '../lib/languages'; // Alterado de .ts para apenas .ts
 
-export default function SelectGroup({ onChange }) {
-    const customStyles = {
-    control: (provided) => ({
+export default function SelectGroup({ onChange }: { onChange: (value: any) => void }) { // Tipando explicitamente o parâmetro onChange
+  const customStyles = {
+    control: (provided: any) => ({
       ...provided,
-      width: 300, // Defina a largura desejada aqui
+      width: 300,
     }),
-    option: (provided, state) => ({
+    option: (provided: any, state: any) => ({
       ...provided,
       backgroundColor: state.isFocused ? '#2648ff' : 'white',
-      color: state.isSelected ? 'black' : 'black',
-      color: state.isFocused ? 'white' : 'black',
+      color: state.isSelected ? 'black' : state.isFocused ? 'white' : 'black', // Mantendo duas configurações de cor para a propriedade color
       '&:hover': {
         backgroundColor: '#2648ff',
         color: 'white'
       },
     }),
-    // Estilo das opções selecionadas
-    singleValue: (provided) => ({
+    singleValue: (provided: any) => ({
       ...provided,
-      color: 'black', // Cor do texto da opção selecionada
+      color: 'black',
     })
-  }
+  };
 
-  return <Select options={languages} styles={customStyles} onChange={onChange}/>
-  
+  return <Select options={languages} styles={customStyles} onChange={onChange}/>;
 }
+
